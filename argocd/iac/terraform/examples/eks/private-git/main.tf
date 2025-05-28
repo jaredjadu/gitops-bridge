2a91e04f566e78559a4dbe7f608b93871acdcfdc
Jared <jared.cassidy@jadu.net>
2025-05-28 17:53:40 +0100

    ebs_csi_driver_irsa

--- argocd/iac/terraform/examples/eks/private-git/main.tf
+++ argocd/iac/terraform/examples/eks/private-git/main.tf
@@ -358,3 +358,21 @@ module "velero_backup_s3_bucket" {
 
   tags = local.tags
 }
+
+module "ebs_csi_driver_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.14"
+
+  role_name_prefix = "${local.name}-ebs-csi-driver-"
+
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+
+  tags = local.tags
+}
